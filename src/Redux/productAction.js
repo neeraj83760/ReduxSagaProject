7fcfa757029ec1423664ed29fc5ebf50752baaf2
Neeraj Ghildiyal <56287670+neeraj83760@users.noreply.github.com>
2022-09-12 18:26:22 +0530

    We called an API in this operation using Fetch as a function

--- src/Redux/productAction.js
+++ src/Redux/productAction.js
@@ -1,7 +1,14 @@
 import { PRODUCT_LIST } from "./constant"
 
-export const productList = () =>{
-    let data = 'hello';
+// API call is always an async operation isliye hum function ko async bana dete hai 
+export const productList = async() =>{
+    let data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    
+    // API se data transfer hota hai wo json format me nahi hota isliye humein usse 
+    // JSON format me convert karna hota hai 
+    
+    data = await data.json()
+
     console.warn('Action Called!!', data)
 
      return{
